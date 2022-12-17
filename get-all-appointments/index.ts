960093bfb8e4844b5453387eba40bc25a86ed306
leomozzer <mozzer.ferraz@gmail.com>
2022-12-17 11:36:53 -0300

    Added azure function

--- get-all-appointments/index.ts
+++ get-all-appointments/index.ts
@@ -1,9 +1,7 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
 import { GetAppointments } from '../src/handlers/appointments';
-
 const GetAllAppointments: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-
     const appointments = await GetAppointments()
     context.res = {
         status: 200,
